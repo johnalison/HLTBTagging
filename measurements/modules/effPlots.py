8f21c68035dc22b24b1cbb3c80bb756a9f6847e1
Korbinian Schweiger <korbinian.schweiger@cern.ch>
2017-10-31 08:31:35 +0100

    Update; Backup

---
+++ measurements/modules/effPlots.py
@@ -0,0 +1,142 @@
+import logging
+from ConfigParser import SafeConfigParser
+
+import ROOT
+
+import modules.plotting
+import modules.classes
+import modules.utils
+
+
+
+def makeEffPlot(PlotBaseObj, Sample, numSelection,  outname = None, outputformat = "pdf", label = None, drawEff = True, forceColor = None):
+    styleconfig = SafeConfigParser()
+    styleconfig.read("config/plotting.cfg")
+
+
+    if label is not None and isinstance(label, list):
+        for l in label:
+            if not isinstance(l, ROOT.TLatex):
+                logging.error("All labels are required to be of type ROOT.TLatex")
+                label.remove(l)
+    elif label is not None and not isinstance(label,ROOT.TLatex):
+        logging.warning("The label param should be of type ROOT.TLatex")
+        logging.warning("Ignoring additional label")
+        label = None
+    elif (label is not None and isinstance(label, ROOT.TLatex)) or label is None:
+        pass
+    else:
+        logging.error("Something is happening here... :(")
+    
+        
+    yTitle = "Effciency"
+    
+    #In principle both PlotBaseObjs shoud have the same binning
+    binning = PlotBaseObj.binning
+
+    
+    
+    hdenominator = modules.plotting.getHistoFromTree(Sample.tree, PlotBaseObj.variable, binning, "({0} && {1})".format(PlotBaseObj.selection, Sample.selection))
+    hnumerator = modules.plotting.getHistoFromTree(Sample.tree, PlotBaseObj.variable, binning, "({0} && {1} && {2})".format(PlotBaseObj.selection, Sample.selection, numSelection))
+
+    if forceColor is not None:
+        logging.debug("Forcing color: {0}".format(forceColor))
+        color = forceColor
+    else:
+        color = PlotBaseObj.color
+    
+    modules.plotting.setStyle(hdenominator, "Line", color, PlotBaseObj.xTitle, yTitle)
+    modules.plotting.setStyle(hnumerator, "Line", color, PlotBaseObj.xTitle, yTitle)
+
+    grEff = modules.plotting.makeEffGraph(hnumerator, hdenominator, yTitle)
+
+    if drawEff:
+        canvas = modules.plotting.getCanvas()
+        canvas.cd()
+        
+        grEff.Draw("AP")
+        grEff.GetHistogram().SetMaximum(styleconfig.getfloat("Efficiency", "yMax"))
+        grEff.GetHistogram().SetMinimum(0.0)
+        grEff.GetXaxis().SetRangeUser(binning[1], binning[2])
+        grEff.Draw("AP")
+        
+        if label is not None:
+            if isinstance(label, list):
+                for l in label:
+                    l.Draw("same")
+            else:
+                label.Draw("same")
+        CMSL1, CMSL2 = modules.utils.getCMStext()
+        CMSL1.Draw("same")
+        CMSL2.Draw("same")
+
+        if outname is not None:
+            modules.utils.savePlot(canvas, outname, outputformat)
+
+    return grEff
+
+    
+def makeEffSCompPlot(PlotBaseObj, Samples, numSelection, outname = None, outputformat = "pdf", label = None):
+    styleconfig = SafeConfigParser()
+    styleconfig.read("config/plotting.cfg")
+
+    binning = PlotBaseObj.binning
+    
+    if label is not None and isinstance(label, list):
+        for l in label:
+            if not isinstance(l, ROOT.TLatex):
+                logging.error("All labels are required to be of type ROOT.TLatex")
+                label.remove(l)
+    elif label is not None and not isinstance(label,ROOT.TLatex):
+        logging.warning("The label param should be of type ROOT.TLatex")
+        logging.warning("Ignoring additional label")
+        label = None
+    elif (label is not None and isinstance(label, ROOT.TLatex)) or label is None:
+        pass
+    else:
+        logging.error("Something is happening here... :(")
+
+    graphs = []
+    forlegend = []
+    colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen-2]
+    #TODO Make something to check num of samples against colors
+    for isample, sample in enumerate(Samples):
+        graphs.append(makeEffPlot(PlotBaseObj, sample, numSelection, drawEff = False, forceColor = colors[isample]))
+        forlegend.append((graphs[isample], sample.legend[0], "p"))
+    
+    canvas = modules.plotting.getCanvas()
+    canvas.cd()
+
+    logging.debug("Drawing graph 0")
+    graphs[0].Draw("AP")
+    graphs[0].GetHistogram().SetMaximum(styleconfig.getfloat("Efficiency", "yMax"))
+    graphs[0].GetHistogram().SetMinimum(0.0)
+    graphs[0].GetXaxis().SetRangeUser(binning[1], binning[2])
+    graphs[0].Draw("AP")
+
+    for i in range(1, len(graphs)):
+        logging.debug("Drawing graph {0}".format(i))
+        graphs[i].Draw("PSame")
+     
+    if label is not None:
+        if isinstance(label, list):
+            for l in label:
+                l.Draw("same")
+        else:
+            label.Draw("same")
+    CMSL1, CMSL2 = modules.utils.getCMStext()
+    CMSL1.Draw("same")
+    CMSL2.Draw("same")
+
+    logging.debug("Making Legend")
+    xstart = PlotBaseObj.LegendPosition[0]
+    ystart = PlotBaseObj.LegendPosition[1]
+    xend = PlotBaseObj.LegendPosition[2]
+    yend = PlotBaseObj.LegendPosition[3]
+    
+    leg = modules.utils.getLegend(forlegend, xstart, ystart, xend, yend)
+    leg.Draw("")
+    
+    
+    if outname is not None:
+        modules.utils.savePlot(canvas, outname, outputformat)
--- measurements/modules/utils.py
+++ measurements/modules/utils.py
@@ -2,6 +2,8 @@ import logging
 import logging.config
 import os
 import json
+import ROOT
+from ConfigParser import SafeConfigParser
 
 def setup_logging( default_path='config/logging.json', default_level=logging.INFO,
                    logname = "output", errname = "error", loglevel = 20):
@@ -27,3 +29,136 @@ def savePlot(canvas, outputname, outputformat):
     #TODO: Make something with multipage pdf support
     logging.info("Writing file {0}.{1}".format(outputname, outputformat))
     canvas.Print("{0}.{1}".format(outputname, outputformat))
+
+
+
+def getLegend(ObjetList, xstart, ystart, xend, yend, usingPlotBase = False):
+    leg = ROOT.TLegend(xstart, ystart, xend, yend)
+    if usingPlotBase:
+        for Object, histo in ObjetList:
+            if len(Object.legend) == 1:
+                leg.AddEntry(histo, Object.legend[0], Object.legendstyle)
+    else:
+        for  obj, text, style in ObjetList:
+            leg.AddEntry(obj, text, style)        
+
+    leg.SetBorderSize(0)
+    leg.SetTextFont(42)
+    leg.SetFillStyle(0)
+            
+    return leg
+
+
+def getCMStext(WiP = True, additionaltext = ""):
+    styleconfig = SafeConfigParser()
+    #logging.debug("Loading style config")
+    styleconfig.read("config/plotting.cfg")
+
+    CMSscale = styleconfig.getfloat("CMSLabel","cmsScale")
+    
+    if additionaltext not in ["", "Prelim","Supp","Sim"]:
+        logging.warning("CMS additinal text required in {0}".format(["WiP","Prelim","Supp","Sim"]))
+        logging.warning("Ignoring additional Text")
+
+    firstline = '#scale['+str(CMSscale)+']{#bf{CMS}}'
+    if additionaltext == "Sim":
+        firstline += " #it{Simulation}"
+    elif additionaltext == "Supp":
+        firstline += " #it{Supplementary}"
+    elif additionaltext == "Prelim":
+        firstline += " #it{Preliminary}"
+    else:
+        pass
+
+    xstart = styleconfig.getfloat("CMSLabel","xStart")
+    
+    cmsfistLine = ROOT.TLatex(xstart, styleconfig.getfloat("CMSLabel","yStartFirstLine"), firstline)
+    cmsfistLine.SetTextFont(42)
+    cmsfistLine.SetTextSize(styleconfig.getfloat("CMSLabel","textsize"))
+    cmsfistLine.SetNDC()
+
+    if WiP:
+        cmssecondline = ROOT.TLatex(xstart, styleconfig.getfloat("CMSLabel","yStartSecondLine"), '#it{work in progress}')
+    else:
+        cmssecondline = ROOT.TLatex(xstart, styleconfig.getfloat("CMSLabel","yStartSecondLine"), '')
+    cmssecondline.SetTextFont(42)
+    cmssecondline.SetTextSize(styleconfig.getfloat("CMSLabel","textsize"))
+    cmssecondline.SetNDC()
+
+    return cmsfistLine, cmssecondline
+
+def getLabel(text, xstart, pos = "top", scale = 1):
+    if pos in ["top", "topSup", "under"]:
+        postouse = pos
+    elif isinstance(pos, float) or isinstance(pos, int):
+        postouse = float(pos)
+    else:
+        logging.warning("Position parameter required in {0} or of type float/int".format(["top", "topSup", "under"]))
+        logging.warning("Falling back to top position")
+        postouse = "top"
+
+        
+    styleconfig = SafeConfigParser()
+    styleconfig.read("config/plotting.cfg")
+
+    if pos == "top":
+        ystart = styleconfig.getfloat("GeneralLabel","topPos")
+    elif pos == "topSup":
+        ystart = styleconfig.getfloat("GeneralLabel","topSupPos")
+        scale = scale * styleconfig.getfloat("GeneralLabel","topSupScale")
+    else:
+        ystart = styleconfig.getfloat("GeneralLabel","underPos")
+
+    label = ROOT.TLatex(xstart, ystart, '#scale['+str(scale)+']{'+text+'}')
+        
+    label.SetTextFont(42)
+    label.SetTextSize(styleconfig.getfloat("CMSLabel","textsize"))
+    label.SetNDC()
+
+    return label
+
+
+def getAxisTitle(variable, number, order = "pt"):
+    numberDictPt = {0 : "leading",
+                    1 : "second",
+                    2 : "third",
+                    3 : "fourth",
+                    4 : "fifth",
+                    5 : "sixth",
+                    6 : "seventh"}
+    numberDictCSV = {0 : "",
+                     1 : "second",
+                     2 : "third",
+                     3 : "fourth",
+                     4 : "fifth",
+                     5 : "sixth",
+                     6 : "seventh"}
+
+    nicevars = {"pt" : "p_{T}",
+                "csv" : "CSV Value",
+                "deepcsv" : "DeepCSV Value"}
+
+    if not variable in nicevars.keys():
+        logging.waring("Variables not in dict with nice names. Just using the variable")
+        nicename = variable
+    else:
+        nicename = nicevars[variable]
+
+    if not number in numberDictPt.keys():
+        logging.waring("Variables not in dict with nice number Just using the number")
+        nicenumber = number
+    else:
+        if numberDictPt:
+            nicenumber = numberDictPt[number]
+        else:
+            nicenumber = numberDictCSV[number]
+            
+    if order == "pt":
+        return "{0} of {1} jet".format(nicename, nicenumber)
+    elif order == "csv":
+        return "{0} of jet with {1} highest CSV".format(nicename, nicenumber)
+    elif order == "deepcsv":
+        return "{0} of jet with {1} highest DeepCSV".format(nicename, nicenumber)
+    else:
+        logging.error("Order not defined. Returning empty sting")
+        return ""
